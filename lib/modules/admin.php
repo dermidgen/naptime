89f254a37420ddd7148bf66533a06d123c2d9fa1
Danny Graham <good.midget@gmail.com>
2010-06-22 04:40:04 -0700

    added basic template for admin settings page

--- lib/modules/admin.php
+++ lib/modules/admin.php
@@ -24,8 +24,20 @@ class admin extends \PASL\Web\Simpl\Page
 		$this->mainNav->addMenuItem(new \naptime\MainNavItem('Documents', 'Manage Documents', $basepath.'/docs', null));
 		$this->mainNav->addMenuItem(new \naptime\MainNavItem('Navigation', 'Manage Menus &amp; Links', $basepath.'/navigation', null));
 		
-		
 		$this->mainNav->selectItemByAttribute('link',$_SERVER['REQUEST_URI']);
+	}
+	
+	private function settings()
+	{
+		$this->subNav->hide();
+		\naptime::GetInstance()->pageDescription = 'Manage Naptime! Settings';
+
+		$this->TOKENS['projectName'] = \naptime::GetInstance()->config->project->name;
+		$this->TOKENS['projectTitle'] = \naptime::GetInstance()->config->project->title;
+		$this->TOKENS['projectDescription'] = \naptime::GetInstance()->config->project->description;
+		$this->TOKENS['companyName'] = \naptime::GetInstance()->config->company->name;
+
+		$this->body = $this->loadAndParse('admin/settings.html');
 	}
 	
 	public function run()
@@ -33,7 +45,7 @@ class admin extends \PASL\Web\Simpl\Page
 		switch(\naptime::GetInstance()->getPath(1))
 		{
 			case "settings":
-				$this->body = join('/', \naptime::GetInstance()->getPath(0,2));
+				$this->settings();
 			break;
 			case "pages":
 				$this->body = join('/', \naptime::GetInstance()->getPath(0,2));
